b3645bdf4003f12ff3f7282f0ff1514b650b7271
meghwardayanand <meghwardayanand@gmail.com>
2025-01-29 13:17:17 -0500

    FEAT: Implemented ReLU Optimizer

--- utils/optimizers.py
+++ utils/optimizers.py
@@ -26,3 +26,19 @@ class Sigmoid(Optimizer):
             return result * (1 - result)
 
         return [i*(1 - i) for i in result]
+
+
+class ReLU(Optimizer):
+    @staticmethod
+    def optimize(x):
+        if type(x) in [int, float]:
+            return max(0, x)
+
+        return [max(0, i) for i in x]
+
+    @staticmethod
+    def prime(x):
+        if type(x) in [int, float]:
+            return 1 if x > 0 else 0
+    
+        return [1 if i > 0 else 0 for i in x]
